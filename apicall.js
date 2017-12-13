698bfaea06b062b775aa8b02ef6fadd3eca8b4f4
AmritK2 <amrit.kaur@myob.com>
2017-12-13 17:03:37 +1300

    need to add CORS

--- apicall.js
+++ apicall.js
@@ -1,13 +1,18 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', "https://www.codewars.com/api/v1/users/Amrit2", false);
-xhr.send();
+if ('withCredentials' in xhr)
+{
+    xhr.open('GET', "https://www.codewars.com/api/v1/users/Amrit2", false);
 
-xhr.onreadystatechange = processRequest;
+    xhr.onreadystatechange = processRequest;
 
-function processRequest(e) {
-    if (xhr.readyState === 4 && xhr.status === 200)
-    {
-        var response = JSON.parse(xhr.responseText);
-        alert(response);
+    function processRequest(e) {
+        if (xhr.readyState === 4)
+        {
+            if (xhr.status >= 200 && xhr.status < 400)
+            var response = JSON.parse(xhr.responseText);
+            alert(response);
+        }
+
     }
+xhr.send();
 }
