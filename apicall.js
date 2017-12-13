43978339e5927066ca497a3cb2ad21edc78a7f8e
AmritK2 <amrit.kaur@myob.com>
2017-12-14 10:27:11 +1300

    CORS sorted but getting 404 error

--- apicall.js
+++ apicall.js
@@ -1,18 +1,74 @@
 var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = processRequest();
+function processRequest() {
+    if (xhr.readyState === 4)
+    {
+        if (xhr.status >= 200 && xhr.status < 400)
+        {
+            var response = JSON.parse(xhr.responseText);
+            //document.write(response);
+            alert(response);
+        }
+        else
+            document.write("Error!")
+    }
+
+}
+xhr.open("GET", "https://www.codewars.com/api/v1/users/Amrit2");
+xhr.withCredentials = true;
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.send({'request': 'authentication token'});
+
+/*var xhr = new XMLHttpRequest();
 if ('withCredentials' in xhr)
 {
-    xhr.open('GET', "https://www.codewars.com/api/v1/users/Amrit2", false);
+    xhr.open('GET', "https://www.codewars.com/api/v1/users/Amrit2", true);
 
     xhr.onreadystatechange = processRequest;
 
-    function processRequest(e) {
+    function processRequest() {
         if (xhr.readyState === 4)
         {
             if (xhr.status >= 200 && xhr.status < 400)
-            var response = JSON.parse(xhr.responseText);
-            alert(response);
+            {
+                var response = JSON.parse(xhr.responseText);
+                document.write(response);
+                //alert(response);
+            }
+            else
+                document.write("Error!")
         }
 
     }
 xhr.send();
+}*/
+/*
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ('withCredentials' in xhr)
+    {
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest !== "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS not supported.
+        document.write("Error!")
+    }
+    return xhr;
 }
+
+// Make the actual CORS request.
+function makeCorsRequest() {
+    // This is a sample server that supports CORS.
+    var url = 'https://www.codewars.com/api/v1/users/Amrit2';
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+
+    xhr.send();
+}
+*/
