161350e23f0f86e55397d0d4f48936ccadc2bd32
AmritK2 <amrit.kaur@myob.com>
2017-12-14 16:19:39 +1300

    getting data for multiple usernames

--- apicall.js
+++ apicall.js
@@ -1,20 +1,31 @@
 function getUserInfo() {
-    var uri = "https://www.codewars.com/api/v1/users/Amrit2";
-    var request = new XMLHttpRequest();
-    request.open("GET", uri, true);
-    request.onreadystatechange = function () {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 400) {
-                console.log(JSON.parse(request.response));
-                document.write(JSON.parse(request.response).name);
-                document.write(JSON.parse(request.response).honor);
-                document.write(JSON.parse(request.response).clan);
-                document.write(JSON.parse(request.response).leaderboardPosition);
+    var array = ["Amrit2", "vietanh16", "DawnPiper"];
+    for (var position = 0; position < array.length; position++) {
+        var uri = "https://www.codewars.com/api/v1/users/" + array[position];
+
+        var request = new XMLHttpRequest();
+        request.open("GET", uri, false); // need it to be synchronous as we don't want to go ahead without getting a callbacn
+        request.onreadystatechange = function () {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status == 200) {
+                    console.log(JSON.parse(request.response));
+                    var userName = JSON.parse(request.response).name;
+                    var userHonor = JSON.parse(request.response).honor;
+                    var userClan = JSON.parse(request.response).clan;
+                    var userPosition = JSON.parse(request.response).leaderboardPosition;
+
+                    document.write("<h1>" + "User Name: " + userName + "</h1>");
+                    document.write("<h2>" + "User Honor: " + userHonor + "</h2>");
+                    document.write("<h2>" + "User Clan: " + userClan + "</h2>");
+                    document.write("<h2>" + "Leader Board Position: " + userPosition + "</h2>");
+                }
+                else {
+                    console.log("Error");
+                }
             }
-            else {
-                console.log("Error");
-            }
-        }
-    };
-    request.send();
+        };
+
+        request.send();
+    }
+
 }
