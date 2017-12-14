16f1a9f127e00b1f7b4231195e7f60327f32f78e
AmritK2 <amrit.kaur@myob.com>
2017-12-14 13:21:02 +1300

    server is working fine

--- apicall.js
+++ apicall.js
@@ -5,9 +5,11 @@ function getUserInfo() {
     request.onreadystatechange = function () {
         if (request.readyState === 4) {
             if (request.status >= 200 && request.status < 400) {
-                //console.log(JSON.parse(request.response));
+                console.log(JSON.parse(request.response));
                 document.write(JSON.parse(request.response).name);
                 document.write(JSON.parse(request.response).honor);
+                document.write(JSON.parse(request.response).clan);
+                document.write(JSON.parse(request.response).leaderboardPosition);
             }
             else {
                 console.log("Error");
